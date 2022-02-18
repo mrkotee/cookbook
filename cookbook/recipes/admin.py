37d6e11a852948a039d83b346c973fce8b4fb072
mrkotee <mcvUjB9b>
2022-02-18 20:20:54 +0300

    set admin models

--- cookbook/recipes/admin.py
+++ cookbook/recipes/admin.py
@@ -1,7 +1,44 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient
+from django.db import models
+from django.forms.widgets import CheckboxSelectMultiple
+from .models import Recipe, Ingredient, RecipeDirection
+
+admin.site.register(RecipeDirection)
+# class MyModelAdmin(admin.ModelAdmin):
+#     formfield_overrides = {
+#         models.ManyToManyField: {'widget': CheckboxSelectMultiple},
+#     }
 
 
-admin.site.register(Recipe)
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    fields = ['name']
+    search_fields = ("name", )
+    ordering = ['name']
 
-admin.site.register(Ingredient)
+
+class RecipeDirectionInline(admin.TabularInline):
+    model = RecipeDirection
+    extra = 0
+    min_num = 3
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    readonly_fields = ["created_at",
+                       "updated_at"]
+
+    fields = [
+        ("name", "ingredients"),
+    ]
+    autocomplete_fields = ["ingredients",]
+
+    inlines = [RecipeDirectionInline]
+
+    search_fields = ("name", )
+    list_filter = ("ingredients", )
+
+    list_per_page = 30
+
+    list_select_related = True
+
