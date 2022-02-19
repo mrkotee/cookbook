7fdbe7f3750a4b15ed6c566d6a0d6473992a31a6
mrkotee <mcvUjB9b>
2022-02-19 17:28:29 +0300

    set loaddata in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,7 @@ python manage.py flush --no-input
 python manage.py makemigrations recipes
 python manage.py migrate
 python manage.py collectstatic
-python manage.py parse_recipes
+#python manage.py parse_recipes
+python manage.py loaddata recipes_models
 
 exec "$@"
