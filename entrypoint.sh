15dc6262629108b60fbe3e80c8056ca0f7557065
root <root@evsey.pw>
2022-02-19 16:05:48 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,8 @@ then
 fi
 
 python manage.py flush --no-input
+python manage.py makemigrations recipes
 python manage.py migrate
-python manage.py migrate recipes
 python manage.py parse_recipes
 
 exec "$@"
